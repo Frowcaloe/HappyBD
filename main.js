66d7024ba77c45cbb66f2a1c068e550ea6b9d44d
Frowcaloe <forgame061601@gmail.com>
2025-06-26 08:45:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             $(this).addClass("puff-bubble");
         });
         $("#glow").remove();
-        txt.hide().html("I wish you happy birthday, Boss Reb!").delay(750).fadeIn(300);
+        txt.hide().html("I wish you happy birthday, Henry!").delay(750).fadeIn(300);
         $("#candle").animate(
             {
             opacity: ".5"
